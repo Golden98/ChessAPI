902f59ac55d7400d5ee003a3a9ff2c372bc17c45
Golden98 <brayden.golden@gmail.com>
2023-03-03 10:04:59 -0500

    base setup

---
+++ src/test/java/com/BraydenGolden/chess/ChessAPI/ChessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BraydenGolden.chess.ChessAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
