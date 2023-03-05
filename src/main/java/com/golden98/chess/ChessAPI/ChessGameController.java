9fd55e4867f383a0a93d41062f700e360ccf72f4
Golden98 <brayden.golden@gmail.com>
2023-03-04 21:18:41 -0500

    add move validation and error response

--- src/main/java/com/golden98/chess/ChessAPI/ChessGameController.java
+++ src/main/java/com/golden98/chess/ChessAPI/ChessGameController.java
@@ -1,16 +1,22 @@
 package com.golden98.chess.ChessAPI;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.List;
+
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mediatype.problem.Problem;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpHeaders;
+// import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.HttpStatus;
 
 
@@ -19,14 +25,14 @@ public class ChessGameController {
     
     private final ChessGameRepository repository;
     private final ChessGameModelAssembler assembler;
-    private static final Logger log = LoggerFactory.getLogger(ChessGameController.class);
+    // private static final Logger log = LoggerFactory.getLogger(ChessGameController.class);
 
     ChessGameController(ChessGameRepository repository, ChessGameModelAssembler assembler) {
         this.repository = repository;
         this.assembler = assembler;
     }
 
-    @PostMapping("/chess/api/solo")
+    @PostMapping("/chess/api/solo/create")
     ResponseEntity<?> newGame() {
         // create new game and save it
         EntityModel<SimpleChessGame> entityModel = assembler.toModel(repository.save(new ChessGame()));
@@ -48,11 +54,28 @@ public class ChessGameController {
     ResponseEntity<?> move(@PathVariable long id, @PathVariable String move) {
         // get game from db
         ChessGame chessGame = repository.findById(id).orElseThrow(() -> new ChessGameNotFoundException(id));
-        chessGame.doMove(move);
-        EntityModel<SimpleChessGame> entityModel = assembler.toModel(repository.save(chessGame));
-        return ResponseEntity
-            .status(HttpStatus.OK)
-            .body(entityModel);
-            // TODO check if move is valid
+        // is idempotent since repeated requests will not effect board, only the first will be a valid move
+        List<String> moves = chessGame.getBoard().legalMoves().stream()
+                            .map(m -> m.toString())
+                            .toList();
+        
+        if (moves.contains(move)) {
+            chessGame.doMove(move);
+            EntityModel<SimpleChessGame> entityModel = assembler.toModel(repository.save(chessGame));
+            return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(entityModel);
+        } else {
+            // use error defined in rfc7807, TODO add error type to response
+            ResponseEntity<?> responseEntity = ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                .body(Problem.create()
+                    .withTitle("Method not allowed")
+                    .withStatus(HttpStatus.METHOD_NOT_ALLOWED)
+                    .withDetail("The Move '" + move + "' is invalid for the board id '" + id + "'")
+                    .withInstance(linkTo(methodOn(ChessGameController.class).move(chessGame.getId(), move)).toUri()));
+            return responseEntity;
+        }
     }
 }
